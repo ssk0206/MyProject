6d3d0e2a16c3ed3f44f0e8c0aab0b6bfd62e4499
SSK0206 <coccinelle.sasa@gmail.com>
2019-04-16 18:49:55 +0900

    RegisterApiテストまで

--- config/database.php
+++ config/database.php
@@ -89,7 +89,7 @@ return [
             'driver' => 'sqlite',
             'database' => ':memory:',
             'prefix' => '',
-        ]
+        ],
 
     ],
 
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+//ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
