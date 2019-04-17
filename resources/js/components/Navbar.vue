0eb94a2e42839923de4e4d322ac553a29ec2ad24
SSK0206 <coccinelle.sasa@gmail.com>
2019-04-17 10:43:35 +0900

    chapter5完了

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav class="navbar">
+        <router-link class="navbar__brand" to="/">
+            Vuesplash
+        </router-link>
+        <div class="navbar__menu">
+            <div class="navbar__item">
+                <button class="button">
+                    <i class="icon icon-md-add"></i>
+                    Submit a photo
+                </button>
+            </div>
+            <span class="navbar__item">
+                username
+            </span>
+            <div class="navbar__item">
+                <router-link class="button button--link" to="/login">
+                    Login / Register
+                </router-link>
+            </div>
+        </div>
+    </nav>
+</template>
