b299c2bfcef3644a98e11cd30327bdea6d90f714
SSK0206 <coccinelle.sasa@gmail.com>
2019-04-24 15:52:39 +0900

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // 本番環境(Heroku)でhttpsを強制する
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
     }
 }
